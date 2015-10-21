fba8554f42569eaa89e7a6cbb4d5bf9fb1d111cb
Vincent Iserentant <vincent.iserentant@gmail.com>
2015-10-21 11:41:40 +0200

    update uptobox login url + cleanup

--- uptobox/host.php
+++ uptobox/host.php
@@ -12,7 +12,7 @@ class SynoFileHostingUptobox {
 	private $Password;
 	private $HostInfo;
 	private $UPTO_COOKIE_JAR = '/tmp/uptobox.cookie';
-	private $LOGIN_URL = "https://login.uptobox.com/";
+	private $LOGIN_URL = 'https://login.uptobox.com/logarithme';
 		
 	public function __construct($Url, $Username, $Password, $HostInfo) {
 		$this->Url = $Url;
@@ -26,84 +26,64 @@ class SynoFileHostingUptobox {
 	}
 	
 	public function GetDownloadInfo($ClearCookie) {
-		if($this->performLogin()==LOGIN_FAIL) {
-			$DownloadInfo = array();
-			$DownloadInfo[DOWNLOAD_ERROR] = ERR_REQUIRED_PREMIUM;
-			return $DownloadInfo;
+		if($this->performLogin() === LOGIN_FAIL) {
+			return array(DOWNLOAD_ERROR => ERR_FILE_NO_EXIST);
 		}
 		return $this->getPremiumDownloadLink();
 	}
 	
 	private function performLogin() {
-		$ret = LOGIN_FAIL;
-		//Save cookie file
-		//op=login&redirect=http%3A%2F%2Fuptobox.com%2F&login=&password=&x=32&y=11
-		$PostData = array('op'=>'login',
-						'redirect'=>'http%3A%2F%2Fuptobox.com%2F',
-						'login'=>$this->Username,
-						'password'=>$this->Password
+		$postData = array(
+			'op'=>'login',
+			'redirect'=>'http%3A%2F%2Fuptobox.com%2F',
+			'login'=>$this->Username,
+			'password'=>$this->Password
 		);
-		$queryUrl = $this->LOGIN_URL;
-		$PostData = http_build_query($PostData);
-		$curl = curl_init();
-		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
-		curl_setopt($curl, CURLOPT_POSTFIELDS, $PostData);
-		curl_setopt($curl, CURLOPT_USERAGENT, DOWNLOAD_STATION_USER_AGENT);
-		curl_setopt($curl, CURLOPT_COOKIEJAR, $this->UPTO_COOKIE_JAR);
-		curl_setopt($curl, CURLOPT_HEADER, TRUE);
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-		curl_setopt($curl, CURLOPT_URL, $queryUrl);
-		$LoginInfo = curl_exec($curl);
-		curl_close($curl);
-		
-		//echo $LoginInfo;
-		
-		//xfss is uptobox logged in cookie value
-		if (FALSE != $LoginInfo && file_exists($this->UPTO_COOKIE_JAR)) {
+		$postData = http_build_query($postData);
+
+		$curlOpt = array(
+			CURLOPT_POSTFIELDS => $postData,
+			CURLOPT_SSL_VERIFYPEER => FALSE,
+			CURLOPT_HEADER => TRUE,
+			CURLOPT_COOKIEJAR => $this->UPTO_COOKIE_JAR,
+			CURLOPT_USERAGENT => DOWNLOAD_STATION_USER_AGENT,
+			CURLOPT_RETURNTRANSFER => TRUE
+		);
+
+		$ch = curl_init($this->LOGIN_URL);
+		curl_setopt_array($ch, $curlOpt);
+		$loginInfo = curl_exec($ch);
+		curl_close($ch);
+		if (FALSE !== $loginInfo && file_exists($this->UPTO_COOKIE_JAR)) {
 			$cookieData = file_get_contents ($this->UPTO_COOKIE_JAR);
 			if(strpos($cookieData,'xfss') !== false) {
-				$ret = USER_IS_PREMIUM;
-				return $ret;
+				return USER_IS_PREMIUM;
 			} else {
-				$ret = LOGIN_FAIL;
-				return $ret;
+				return LOGIN_FAIL;
 			}
 		}
-		$ret = LOGIN_FAIL;
-		return $ret;
+		return LOGIN_FAIL;
 	}
 	
 	private function getPremiumDownloadLink() {
-		
-		$ret = false;
-		$curl = curl_init();
-		curl_setopt($curl, CURLOPT_USERAGENT, DOWNLOAD_STATION_USER_AGENT);
-		curl_setopt($curl, CURLOPT_URL, $this->Url);
-		curl_setopt($curl, CURLOPT_COOKIEFILE, $this->UPTO_COOKIE_JAR);
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); 
-		
-		curl_setopt($curl, CURLOPT_HEADER, true);
-		//curl exec has to be called before getinfo
-		$header = curl_exec($curl);
-		$code = curl_getinfo($curl, CURLINFO_REDIRECT_URL);
-		$info = curl_getinfo($curl);
-		$error_code = $info['http_code'];
-		
-		//var_dump(curl_getinfo($curl));
-		//echo $code;
-		//if 302 found in header - file is working and downloadable
-		if ($error_code == 301 || $error_code == 302) { 
-			//echo $info['redirect_url'];
-			$DownloadInfo = array();
-			$DownloadInfo[DOWNLOAD_URL] = $info['redirect_url'];
-			return $DownloadInfo;
+		$curlOpt = array(
+			CURLOPT_USERAGENT => DOWNLOAD_STATION_USER_AGENT,
+			CURLOPT_HEADER => TRUE,
+			CURLOPT_COOKIEFILE => $this->UPTO_COOKIE_JAR,
+			CURLOPT_RETURNTRANSFER => true
+		);
+
+		$ch = curl_init($this->Url);
+		curl_setopt_array($ch, $curlOpt);
+		curl_exec($ch);
+		$info = curl_getinfo($ch);
+		$return_code = $info['http_code'];
+		curl_close($ch);
+
+		if ($return_code === 301 || $return_code === 302) {
+			return array(DOWNLOAD_URL => $info['redirect_url']);
 		}else{
-			//echo "error";
-			$DownloadInfo = array();
-			$DownloadInfo[DOWNLOAD_ERROR] = ERR_FILE_NO_EXIST;
-			return $DownloadInfo;
+			return array(DOWNLOAD_ERROR => ERR_FILE_NO_EXIST);
 		}
-		curl_close($curl);
 	}
 }
-?>
